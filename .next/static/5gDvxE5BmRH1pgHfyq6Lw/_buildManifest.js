9ffe77ca31d00cdb5dcf8b4c139c7091d1f2b29b
EMERIC237 <bestvemeric@gmail.com>
2021-09-28 06:57:05 -0400

    dev installed

---
+++ .next/static/5gDvxE5BmRH1pgHfyq6Lw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95b64a6e-4837f896f516a58e8f77.js","static/chunks/pages/index-4626faa413213a01c4bb.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
